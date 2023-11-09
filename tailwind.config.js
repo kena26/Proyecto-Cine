bb88dc735ed0d6ca2abc67ae4caa27719c1fbf7d
Orzy895 <enrique.wen@utp.ac.pa>
2023-11-09 15:06:32 -0500

    se añade la pantalla de inicio estatico

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./Cine/src/main/resources/static/dist/*.{html,js}"],
   theme: {
-    extend: {},
+    maxHeight:{
+      '1/2': '50%',
+      '3/4': '75%',
+    },
+    extend: {
+      backgroundImage: {
+        'radial-gradient': 'radial-gradient(86.94% 47.67% at 50.02% 52.33%, #431B19 35.42%, #141414 73.96%)',
+      },
+    },
   },
   plugins: [],
 }
